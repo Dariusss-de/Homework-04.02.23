bc77220e4c578731d397b5e90ddf1b341efc2a5a
Dmitry <mangosovdima@mail.ru>
2023-02-06 23:15:15 +0300

    added task1

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Написать программу, которая обменивает элементы первой строки и последней строки
+
+void FillArray(int[,] array)
+{
+    for(int i =0; i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            array[i,j] = new Random().Next(-100,100);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for(int i =0; i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            Console.Write(array[i, j]+"\t");
+        }
+         Console.WriteLine();
+    }
+}
+
+void swapString(int[,] array, int arg)
+{
+    for(int i =0; i<array.GetLength(0);i++)
+    {
+        int temp = array[0,i];
+        array[0,i] = array[arg-1,i];
+        array[arg-1,i] = temp;
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int line = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+int[,] arr = new int[line,column];
+FillArray(arr);
+PrintArray(arr);
+swapString(arr, line);
+PrintArray(arr);
