e096b3e2898fe6cbd08dccfe7db84b55d26a4386
Dmitry <mangosovdima@mail.ru>
2023-02-06 23:15:33 +0300

    added task2

---
+++ Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+void FillArray(int[,] array)
+{
+    for(int i =0; i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            array[i,j] = new Random().Next(-100,100);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for(int i =0; i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        {
+            Console.Write(array[i, j]+"\t");
+        }
+         Console.WriteLine();
+    }
+}
+
+void DescendingOrdering(int[,] array)
+{
+    for(int i =0; i<array.GetLength(0);i++)
+    {
+       for(int j=0;j<array.GetLength(1);j++)
+       {
+          for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+
+       }
+    }
+}
+}
+
+Console.Write("Введите количество строк: ");
+int line = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+int[,] arr = new int[line,column];
+FillArray(arr);
+PrintArray(arr);
+DescendingOrdering(arr);
+PrintArray(arr);
